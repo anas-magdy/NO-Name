1c88bc5b883fe7b8ce9cb8755e7d4fcfc31ffdfd
anas-magdy <anasmagdy80@gmail.com>
2021-04-19 20:09:50 +0200

    Signed-off-by: anas-magdy <anasmagdy80@gmail.com>

---
+++ project it/semiformal/bootstrap/js/belal.js
@@ -0,0 +1,53 @@
+$(document).ready(function()
+{
+    $(".bu-1").on("click",function()
+    {
+        $(".pf-1").slideToggle();
+
+    });
+    
+    $(".bu-2").on("click",function()
+    {
+        $(".pf-2").slideToggle();
+
+    });
+    
+    $(".bu-3").on("click",function()
+    {
+        $(".pf-3").slideToggle();
+
+    });
+    
+    $(".bu-4").on("click",function()
+    {
+        $(".pf-4").slideToggle();
+
+    });
+    
+    $(".bus-1").on("click",function()
+    {
+        $(".ps-1").slideToggle();
+
+    });
+    $(".bus-2").on("click",function()
+    {
+        $(".ps-2").slideToggle();
+
+    });
+    $(".bus-3").on("click",function()
+    {
+        $(".ps-3").slideToggle();
+
+    });
+    $(".bus-4").on("click",function()
+    {
+        $(".ps-4").slideToggle();
+
+    });
+
+});
+function welcome()
+                {
+                    alert("WELCOME");
+                }
+            welcome();
